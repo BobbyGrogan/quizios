cd46de80eeabbb146c85b981641b324af0c0a4f1
Bobby Grogan <bobbydg11@gmail.com>
2023-06-26 21:56:30 -0400

    revised

--- trivia/Components/qandaContainer.swift
+++ trivia/Components/qandaContainer.swift
@@ -4,8 +4,97 @@
 //
 //  Created by Bobby on 6/25/23.
 //
-
 import SwiftUI
+
+struct ansButton: View {
+    let answer: String
+    let correct: Bool
+    let isAnswered: Bool // Added isAnswered property to check if any answer has been selected
+    
+    var body: some View {
+        let backColor: Color = isAnswered ? (correct ? .green : .red) : Color("answerColor")
+        let textColor: Color = isAnswered ? .white : Color("textColor1")
+        
+        Button(action: {}) {
+            Text(answer)
+                .frame(width: 300)
+                .padding()
+                .background(backColor)
+                .foregroundColor(textColor)
+                .cornerRadius(8)
+        }
+      //  .disabled(isAnswered) // Disable button after any answer has been selected
+    }
+}
+
+struct ansButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ansButton(answer: "Hello, World!", correct: true, isAnswered: false)
+    }
+}
+
+
+struct questionBlock: View {
+    var question: String
+
+    var body: some View {
+        Text(question)
+    }
+}
+
+struct questionBlock_Previews: PreviewProvider {
+    static var previews: some View {
+        questionBlock(question: "What is the capital of the United Kingdom?")
+    }
+}
+
+
+struct timerBar: View {
+    @State private var timerValue: CGFloat = 1.0
+    @State private var isTimerRunning = false
+    
+    let timerDuration: TimeInterval
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Rectangle()
+                .frame(height: 20)
+                .foregroundColor(.red)
+            
+            Rectangle()
+                .frame(width: UIScreen.main.bounds.width * timerValue, height: 20)
+                .foregroundColor(.green)
+        }
+        .cornerRadius(10)
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    private func startTimer() {
+        guard !isTimerRunning else { return }
+        
+        isTimerRunning = true
+        
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            withAnimation(.linear(duration: timerDuration)) {
+                timerValue = 0.0
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + timerDuration) {
+                timer.invalidate()
+                isTimerRunning = false
+            }
+        }
+    }
+}
+
+struct timerBar_Previews: PreviewProvider {
+    static var previews: some View {
+        timerBar(timerDuration: 10)
+            .padding()
+    }
+}
 
 struct qandaContainer: View {
     let question: String
@@ -13,35 +102,39 @@ struct qandaContainer: View {
     let correctAnswer: Int
 
     @State private var selectedAnswer: Int?
-
+    @State private var isAnswered: Bool = false
+    @State private var isQuestionDone: Bool = false // Track if the question is done
+    
     var body: some View {
         VStack {
-            questionBlock(question: question)
-
-            ForEach(0..<answers.count) { index in
-                ansButton(answer: answers[index], correct: correctAnswer == index)
+            timerBar(timerDuration: 19)
+            
+            questionBlock(question: isQuestionDone ? "Done" : question) // Update the question text
+            
+            ForEach(answers.indices, id: \.self) { index in
+                ansButton(answer: answers[index], correct: correctAnswer == index, isAnswered: isAnswered)
                     .foregroundColor(answerColor(index: index))
                     .onTapGesture {
-                        selectedAnswer = index
+                        if !isAnswered {
+                            selectedAnswer = index
+                            isAnswered = true
+                            isQuestionDone = true // Set the question as done
+                        }
                     }
+                    .disabled(isAnswered) // Disable button after any answer has been selected
             }
         }
+        .padding()
     }
 
     private func answerColor(index: Int) -> Color {
         if let selectedAnswer = selectedAnswer {
-            if index == selectedAnswer && index == correctAnswer {
+            if correctAnswer == selectedAnswer && isAnswered {
                 return .green
-            } else if index == selectedAnswer && index != correctAnswer {
-                return .red
+            } else if selectedAnswer == index && isAnswered {
+                return .red // Set the selected incorrect answer to red
             }
         }
         return Color("answerColor")
-    }
-}
-
-struct qandaContainer_Previews: PreviewProvider {
-    static var previews: some View {
-        qandaContainer(question: "What is the capital of the United Kingdom?", answers: ["London", "Paris", "Berlin", "Madrid"], correctAnswer: 1)
     }
 }
--- trivia/ContentView.swift
+++ trivia/ContentView.swift
@@ -9,9 +9,80 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            timerBar(timerDuration: 19)
-            qandaContainer(question: "What is the capital of the United Kingdom?", answers: ["London", "Manchester", "Edinburgh", "Belfast"], correctAnswer: 2)
+        VStack(spacing: 20) {
+            VStack(spacing: 50) {
+                HStack {
+                    Spacer()
+                    Image(systemName: "line.3.horizontal")
+                    Spacer()
+                    Text("Title")
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .center)
+                    Spacer()
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color(.blue))
+                .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+                
+                ProgressView(value: 0.9)
+                    .progressViewStyle(LinearProgressViewStyle())
+                    .frame(height: 20)
+                    .foregroundColor(Color(.purple))
+                    .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+                    .padding(.bottom, 20)
+                    .scaleEffect(x: 1, y: 4, anchor: .center)
+            }
+
+            Text("What is the capital of Ireland?")
+                .bold()
+            
+            VStack(spacing: 10) {
+                Button("Galway") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(.green))
+                .cornerRadius(8)
+                .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+                
+                Button("Dublin") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(.green))
+                .cornerRadius(8)
+                .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+                
+                Button("Cork") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(.green))
+                .cornerRadius(8)
+                .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+                
+                Button("Belfast") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(.green))
+                .cornerRadius(8)
+                .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+            }
+            .padding()
+            
+            ProgressView(value: 0.5)
+                .progressViewStyle(LinearProgressViewStyle())
+                .frame(height: 20)
+                .foregroundColor(Color(.orange))
+                .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+                .scaleEffect(x: 1, y: 4, anchor: .center)
+            Spacer()
         }
         .padding()
     }
